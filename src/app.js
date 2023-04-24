dd81c1b0f98f2d887517d6eec537c97b58179225
faun3 <antonioculcea@gmail.com>
2023-04-24 22:51:50 +0300

    remove wrong import and call functions properly

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-const app = appFactory();
 import { appFactory } from "./todoApp";
 import { appViewFactory } from "./DOMstuff";
 
+let projArr = [];
+
+const app = appFactory(projArr);
 const appView = appViewFactory(app.projArr);
 
 console.table(app.projArr);
